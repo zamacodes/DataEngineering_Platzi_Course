d61fef0430b8d970b2ea03ee80010220d2341a70
ZamarronUbuntuLenovo <eduardo.zamarron.m@gmail.com>
2020-04-04 18:42:46 -0600

    Missing Data and Additional clean up

--- pandas/newspaper_recipe.py
+++ pandas/newspaper_recipe.py
@@ -4,7 +4,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 from  urllib.parse import urlparse
 import re
-
+import hashlib
 logger = logging.getLogger(__name__)
 
 def _read_data(filename):
@@ -34,6 +34,47 @@ def _eliminate_tags(df):
         re.sub(tag_regex, '', _)
     return df
 
+def _fill_missing_titles(df):
+    logger.info('Filling missing titles')
+    missing_titles_mask = df['title'].isna()
+    missing_titles = (df[missing_titles_mask]['url']
+                    .str.extract(r'(?P<missing_titles>[^/]+)$')
+                    .applymap(lambda title: title.replace('-',' '))
+                    .applymap(lambda title: title.replace('.html','')) 
+                    )
+    df.loc[missing_titles_mask, 'title'] = missing_titles.loc[:,'missing_titles']
+    return df
+
+def _generate_uids_for_rows(df):
+    logger.info(f'Generating uids')
+    uids = (df
+            .apply(lambda row: hashlib.md5(bytes(row['url'].encode())),
+                axis = 'columns')
+            .apply(lambda hash_object: hash_object.hexdigest())
+            ) 
+    df['uid'] = uids
+    return df.set_index('uid')
+
+def _remove_new_lines_from_body(df):
+    logger.info(f'Removing line breaks and tabs from body')
+    stripped_body = (df
+                     .apply(lambda row: row['body'], axis = 'columns')
+                     .apply(lambda body: list(body))
+                     .apply(lambda letters: list(map(lambda letter: letter.replace ('\n',''), letters)))
+                     .apply(lambda letters_list: ''.join(letters_list))
+                     
+                )
+    df['body'] = stripped_body
+    stripped_body = (df
+                    .apply(lambda row: row['body'], axis = 'columns')
+                    .apply(lambda body: list(body))
+                    .apply(lambda letters: list(map(lambda letter: letter.replace ('\t',''), letters)))
+                    .apply(lambda letters_list: ''.join(letters_list))
+                    
+            )
+    df['body'] = stripped_body
+    return df
+
 
 def main(filename):
     logger.info('Starting cleaning process')
@@ -43,6 +84,10 @@ def main(filename):
     df = _add_newspaper_uid_column(df,newspaper_uid)
     df = _extract_host(df)
     df = _eliminate_tags(df)
+    df = _fill_missing_titles(df)
+    df = _generate_uids_for_rows(df)
+    df = _remove_new_lines_from_body(df)
+
     return df
 
 
